a5f807974f9902a9b5d6da58fba449a1d7b4c55b
itsprathapkreddy <kprathapreddy1997@gmail.com>
2022-05-11 16:00:27 -0400

    added react-helmet

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import Projects from './Components/Projects';
 import Footer from './Components/Footer';
 import Loader from './Components/Loader';
 import { useEffect, useState } from 'react';
+import Helmet from 'react-helmet';
 function App() {
 	const [loadTimer, setLoadTimer] = useState(true);
 	useEffect(() => {
@@ -19,7 +20,25 @@ function App() {
 	}, []);
 	return (
 		<Container fluid className='appContainer'>
-			{false ? (
+			<Helmet>
+				<title>Prathap Reddy</title>
+				<meta property='og:title' content='Prathap Reddy' />
+				<meta
+					property='og:image'
+					content='http://drive.google.com/uc?export=view&id=1r_-NwxYfRYZ0G4-tquOv_ggL240j5xxH'
+				/>
+				<meta
+					property='og:description'
+					content='A portfolio website to showcase all my work'
+				/>
+				<meta property='og:url' content='https://itsprathapkreddy.com' />
+				<meta name='twitter:card' content='summary_large_image' />
+				<meta
+					name='description'
+					content='A portfolio website to showcase all my work'
+				/>
+			</Helmet>
+			{loadTimer ? (
 				<Loader />
 			) : (
 				<div className='scrollContainer'>
