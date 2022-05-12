3d281138eaaa70611d1bd13ad88a7c0f8092fd86
itsprathapkreddy <kprathapreddy1997@gmail.com>
2022-05-11 22:34:26 -0400

    animations added

--- src/Components/Experience.js
+++ src/Components/Experience.js
@@ -2,6 +2,9 @@ import { useState } from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 import { Waypoint } from 'react-waypoint';
 
+import { Zoom, Fade, Slide } from 'react-reveal';
+import Shake from 'react-reveal/Shake';
+
 import '../App.css';
 const Experience = () => {
 	const [company, setCompany] = useState('TCS');
@@ -13,73 +16,79 @@ const Experience = () => {
 			/>
 
 			<Row>
-				<div className='separator'>
-					<h2>Work Places</h2>
-				</div>
+				<Zoom top>
+					<div className='separator'>
+						<h2>Work Places</h2>
+					</div>
+				</Zoom>
 			</Row>
-			<Row className='expHeader' style={{ paddingBottom: '0px' }}>
-				<Col
-					className={company === 'TCS' ? 'expShow' : 'expHide'}
-					xs={6}
-					onClick={() => setCompany('TCS')}>
-					TCS
-				</Col>
-				<Col
-					className={company === 'MBP' ? 'expShow' : 'expHide'}
-					xs={6}
-					onClick={() => setCompany('MBP')}>
-					MBP Solutions
-				</Col>
-			</Row>
-			<Row className='expData' style={{ paddingTop: '0px' }}>
-				<div className={company !== 'TCS' ? 'hidden' : 'compDesContainer'}>
-					<h4>
-						Front End Developer
-						<span style={{ color: 'var(--green)' }}>
-							@Tata Consultancy Services
-						</span>
-					</h4>
-					<div>
+			<Slide right>
+				<Row className='expHeader' style={{ paddingBottom: '0px' }}>
+					<Col
+						className={company === 'TCS' ? 'expShow' : 'expHide'}
+						xs={6}
+						onClick={() => setCompany('TCS')}>
+						TCS
+					</Col>
+					<Col
+						className={company === 'MBP' ? 'expShow' : 'expHide'}
+						xs={6}
+						onClick={() => setCompany('MBP')}>
+						MBP Solutions
+					</Col>
+				</Row>
+			</Slide>
+			<Slide left>
+				<Row className='expData' style={{ paddingTop: '0px' }}>
+					<div className={company !== 'TCS' ? 'hidden' : 'compDesContainer'}>
+						<h4>
+							Front End Developer
+							<span style={{ color: 'var(--green)' }}>
+								@Tata Consultancy Services
+							</span>
+						</h4>
+						<div>
+							<ul>
+								<li style={{ display: 'list-item' }}>
+									Analyzed the current system and came up with required design
+									changes to suit the employee requirements.
+								</li>
+
+								<li style={{ display: 'list-item' }}>
+									Designed and developed very complex and large web pages and
+									modals using ReactJS, JavaScript, HTML5, and CSS3.
+								</li>
+
+								<li style={{ display: 'list-item' }}>
+									Participated in code review sessions and system design
+									discussions. Interacted with Product Managers to fine-tune
+									user stories and with the testing team to approve functional
+									specifications and test cases.
+								</li>
+							</ul>
+						</div>
+					</div>
+					<div className={company !== 'MBP' ? 'hidden' : 'compDesContainer'}>
+						<h4>
+							WordPress administrator
+							<span style={{ color: 'var(--green)' }}> @MBP Solutions</span>
+						</h4>
 						<ul>
-							<li style={{ display: 'list-item' }}>
-								Analyzed the current system and came up with required design
-								changes to suit the employee requirements.
+							<li>Part of UI Design for the entire website.</li>
+
+							<li>
+								Created multiple designs using the Elementor page builder, by
+								adding custom HTML, CSS, and JS.
 							</li>
 
-							<li style={{ display: 'list-item' }}>
-								Designed and developed very complex and large web pages and
-								modals using ReactJS, JavaScript, HTML5, and CSS3.
-							</li>
-
-							<li style={{ display: 'list-item' }}>
-								Participated in code review sessions and system design
-								discussions. Interacted with Product Managers to fine-tune user
-								stories and with the testing team to approve functional
-								specifications and test cases.
+							<li>
+								Created company’s presence on Google My Business. Optimized the
+								company’s website for Search Engines using google analytics.
 							</li>
 						</ul>
 					</div>
-				</div>
-				<div className={company !== 'MBP' ? 'hidden' : 'compDesContainer'}>
-					<h4>
-						WordPress administrator
-						<span style={{ color: 'var(--green)' }}> @MBP Solutions</span>
-					</h4>
-					<ul>
-						<li>Part of UI Design for the entire website.</li>
-
-						<li>
-							Created multiple designs using the Elementor page builder, by
-							adding custom HTML, CSS, and JS.
-						</li>
-
-						<li>
-							Created company’s presence on Google My Business. Optimized the
-							company’s website for Search Engines using google analytics.
-						</li>
-					</ul>
-				</div>
-			</Row>
+				</Row>
+			</Slide>
 		</Container>
 	);
 };
--- src/Components/HeroSec.js
+++ src/Components/HeroSec.js
@@ -1,6 +1,7 @@
 import './heroSec.css';
 import { Container, Row, Col } from 'react-bootstrap';
-
+import { Fade, Slide } from 'react-reveal';
+import Pulse from 'react-reveal/Pulse';
 const HeroSec = () => {
 	return (
 		<Container fluid className='heroSec d-flex align-items-center scrollItem'>
@@ -8,20 +9,28 @@ const HeroSec = () => {
 				<Row>
 					<Col>
 						<div className='heroSecContent'>
-							<div className='introText'>Hey there, this is</div>
-							<div className='heroName'>Prathap Reddy</div>
-							<div className='heroDes'>
-								I build interactive websites that run across platforms & devices
-							</div>
-
-							<div>
-								<a
-									href='https://drive.google.com/uc?export=download&id=1DIrKHX621neidNSzhdfSbAu0LIu1UThr'
-									target='_blank'
-									className='downloadResume'>
-									Download RESUME
-								</a>
-							</div>
+							<Fade left>
+								<div className='introText'>Hey there, this is</div>
+							</Fade>
+							<Fade right big cascade>
+								<div className='heroName'>Prathap Reddy</div>
+							</Fade>
+							<Slide left>
+								<div className='heroDes'>
+									I build interactive websites that run across platforms &
+									devices
+								</div>
+							</Slide>
+							<Pulse>
+								<div>
+									<a
+										href='https://drive.google.com/uc?export=download&id=1DIrKHX621neidNSzhdfSbAu0LIu1UThr'
+										target='_blank'
+										className='downloadResume'>
+										Download RESUME
+									</a>
+								</div>
+							</Pulse>
 						</div>
 					</Col>
 				</Row>
