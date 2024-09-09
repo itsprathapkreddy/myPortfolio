a3906d8faa6960f30bad143082a057dd2f123eb5
itsprathapkreddy <kprathapreddy1997@gmail.com>
2024-09-08 23:31:22 -0400

    updated

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,19 +1,19 @@
-import './contact.css';
-import { Container, Row, Col } from 'react-bootstrap';
+import "./contact.css";
+import { Container, Row, Col } from "react-bootstrap";
 const Contact = () => {
-	return (
-		<Container fluid className='contactCont scrollItem'>
-			<Container>
-				<div className='separator'>
-					<h2>Get In Touch</h2>
-				</div>
-				<div style={{ padding: '30px' }}>
-					<h4>Leave an email, I will get back to you at the earliest.</h4>
-					<h2 className='aboutEmail'>contact@itsprathapkreddy.com</h2>
-				</div>
-			</Container>
-		</Container>
-	);
+    return (
+        <Container fluid className="contactCont scrollItem">
+            <Container>
+                <div className="separator">
+                    <h2>Get In Touch</h2>
+                </div>
+                <div style={{ padding: "30px" }}>
+                    <h4>Leave an email, I will get back to you at the earliest.</h4>
+                    <h2 className="aboutEmail">*****</h2>
+                </div>
+            </Container>
+        </Container>
+    );
 };
 
 export default Contact;
--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -1,146 +1,118 @@
-import { Container, Row, Col, Button } from 'react-bootstrap';
-import { SiGithub } from 'react-icons/si';
-import { HiExternalLink } from 'react-icons/hi';
-import '../App.css';
+import { Container, Row, Col, Button } from "react-bootstrap";
+import { SiGithub } from "react-icons/si";
+import { HiExternalLink } from "react-icons/hi";
+import "../App.css";
 
-import { Fade, Slide, Zoom } from 'react-reveal';
-import Shake from 'react-reveal/Shake';
-import Demo from '../assets/fincal.png';
-import typeTestImg from '../assets/typetest.png';
-import expmngr from '../assets/expmngr.png';
+import { Fade, Slide, Zoom } from "react-reveal";
+import Shake from "react-reveal/Shake";
+import Demo from "../assets/fincal.png";
+import typeTestImg from "../assets/typetest.png";
+import expmngr from "../assets/expmngr.png";
 const Projects = () => {
-	return (
-		<Container className='projCont scrollItem'>
-			<Zoom top>
-				<div className='separator'>
-					<h2 className='projContHead'>Projects</h2>
-				</div>
-			</Zoom>
-			<Slide right>
-				<Row className='indProj'>
-					<Col sm={6} style={{ padding: '0px' }}>
-						<img src={Demo} className='ProjectImage' />
-					</Col>
-					<Col sm={6} className='indProjDes'>
-						<h2 style={{ padding: '10px', marginBottom: '20px' }}>Fincal</h2>
-						<h5>
-							A comprehensive financial calculator app, which is built with
-							vanilla JavaScript, HTML, and CSS using the bootstrap framework.
-							This Web app is converted into a PWA using pwabuilder.
-						</h5>
-						<Row style={{ padding: '10px 0px' }}>
-							<Col xs={6}>
-								<Button variant='primary' className='projBtn'>
-									<a
-										href='https://github.com/itsprathapkreddy/fincal'
-										target='_blank'
-										style={{ color: 'inherit', textDecoration: 'none' }}>
-										<SiGithub style={{ fontSize: '24px' }} />
-										<span>&nbsp;&nbsp;&nbsp;Github</span>
-									</a>
-								</Button>
-							</Col>
-							<Col xs={6}>
-								<Button variant='success' className='projBtn'>
-									<a
-										href='https://fincal.in/pages/home.html'
-										target='_blank'
-										style={{ color: 'inherit', textDecoration: 'none' }}>
-										<HiExternalLink style={{ fontSize: '24px' }} />
-										<span>&nbsp;&nbsp;&nbsp;Live</span>
-									</a>
-								</Button>
-							</Col>
-						</Row>
-					</Col>
-				</Row>
-			</Slide>
-			<Slide left>
-				<Row className='indProj medQuRevCol'>
-					<Col sm={6} className='indProjDes'>
-						<h2 style={{ padding: '10px', marginBottom: '20px' }}>
-							Typing Test
-						</h2>
-						<h5>
-							The faster you type, the faster you communicate with others. With
-							typing test, you can check your live speed i.e., Words per Min,
-							accuracy, and get a detailed report on which words you typed
-							wrong.
-						</h5>
-						<Row style={{ padding: '10px 0px' }}>
-							<Col xs={6}>
-								<Button variant='primary' className='projBtn'>
-									<a
-										href='https://github.com/itsprathapkreddy/typingtest'
-										target='_blank'
-										style={{ color: 'inherit', textDecoration: 'none' }}>
-										<SiGithub style={{ fontSize: '24px' }} />
-										<span>&nbsp;&nbsp;&nbsp;Github</span>
-									</a>
-								</Button>
-							</Col>
-							<Col xs={6}>
-								<Button variant='success' className='projBtn'>
-									<a
-										href='https://itsprathapkreddy.github.io/typingtest/'
-										target='_blank'
-										style={{ color: 'inherit', textDecoration: 'none' }}>
-										<HiExternalLink style={{ fontSize: '24px' }} />
-										<span>&nbsp;&nbsp;&nbsp;Live</span>
-									</a>
-								</Button>
-							</Col>
-						</Row>
-					</Col>
-					<Col sm={6} style={{ padding: '0px' }}>
-						<img src={typeTestImg} className='ProjectImage' />
-					</Col>
-				</Row>
-			</Slide>
-			<Slide right>
-				<Row className='indProj'>
-					<Col sm={6} style={{ padding: '0px' }}>
-						<img src={expmngr} className='ProjectImage' />
-					</Col>
-					<Col sm={6} className='indProjDes'>
-						<h2 style={{ padding: '10px', marginBottom: '20px' }}>
-							Expense Manager
-						</h2>
-						<h5>
-							Managing everyday expenses is the first step to mastering your
-							financial stability. This Expense manager lets you add your
-							everyday expenses and categorize them and sort them by price every
-							month. It also comes with an interactive dashboard of your monthly
-							and yearly expenses.
-						</h5>
-						<Row style={{ padding: '10px 0px' }}>
-							<Col xs={6}>
-								<Button variant='primary' className='projBtn'>
-									<a
-										href='https://github.com/itsprathapkreddy/ExpenseManager'
-										target='_blank'
-										style={{ color: 'inherit', textDecoration: 'none' }}>
-										<SiGithub style={{ fontSize: '24px' }} />
-										<span>&nbsp;&nbsp;&nbsp;Github</span>
-									</a>
-								</Button>
-							</Col>
-							<Col xs={6}>
-								<Button variant='success' className='projBtn'>
-									<a
-										href='https://expensemanager-993c2.web.app/dashboard'
-										target='_blank'
-										style={{ color: 'inherit', textDecoration: 'none' }}>
-										<HiExternalLink style={{ fontSize: '24px' }} />
-										<span>&nbsp;&nbsp;&nbsp;Live</span>
-									</a>
-								</Button>
-							</Col>
-						</Row>
-					</Col>
-				</Row>
-			</Slide>
-		</Container>
-	);
+    return (
+        <Container className="projCont scrollItem">
+            <Zoom top>
+                <div className="separator">
+                    <h2 className="projContHead">Projects</h2>
+                </div>
+            </Zoom>
+            <Slide right>
+                <Row className="indProj">
+                    <Col sm={6} style={{ padding: "0px" }}>
+                        <img src={Demo} className="ProjectImage" />
+                    </Col>
+                    <Col sm={6} className="indProjDes">
+                        <h2 style={{ padding: "10px", marginBottom: "20px" }}>Fincal</h2>
+                        <h5>
+                            A comprehensive financial calculator app, which is built with vanilla JavaScript, HTML, and CSS using the bootstrap framework. This Web app is converted
+                            into a PWA using pwabuilder.
+                        </h5>
+                        <Row style={{ padding: "10px 0px" }}>
+                            <Col xs={6}>
+                                <Button variant="primary" className="projBtn">
+                                    <a href="https://github.com/itsprathapkreddy/fincal" target="_blank" style={{ color: "inherit", textDecoration: "none" }}>
+                                        <SiGithub style={{ fontSize: "24px" }} />
+                                        <span>&nbsp;&nbsp;&nbsp;Github</span>
+                                    </a>
+                                </Button>
+                            </Col>
+                            <Col xs={6}>
+                                <Button variant="success" className="projBtn">
+                                    <a href="https://financial-calculators-site.vercel.app/pages/home.html" target="_blank" style={{ color: "inherit", textDecoration: "none" }}>
+                                        <HiExternalLink style={{ fontSize: "24px" }} />
+                                        <span>&nbsp;&nbsp;&nbsp;Live</span>
+                                    </a>
+                                </Button>
+                            </Col>
+                        </Row>
+                    </Col>
+                </Row>
+            </Slide>
+            <Slide left>
+                <Row className="indProj medQuRevCol">
+                    <Col sm={6} className="indProjDes">
+                        <h2 style={{ padding: "10px", marginBottom: "20px" }}>Typing Test</h2>
+                        <h5>
+                            The faster you type, the faster you communicate with others. With typing test, you can check your live speed i.e., Words per Min, accuracy, and get a
+                            detailed report on which words you typed wrong.
+                        </h5>
+                        <Row style={{ padding: "10px 0px" }}>
+                            <Col xs={6}>
+                                <Button variant="primary" className="projBtn">
+                                    <a href="https://github.com/itsprathapkreddy/typingtest" target="_blank" style={{ color: "inherit", textDecoration: "none" }}>
+                                        <SiGithub style={{ fontSize: "24px" }} />
+                                        <span>&nbsp;&nbsp;&nbsp;Github</span>
+                                    </a>
+                                </Button>
+                            </Col>
+                            <Col xs={6}>
+                                <Button variant="success" className="projBtn">
+                                    <a href="https://itsprathapkreddy.github.io/typingtest/" target="_blank" style={{ color: "inherit", textDecoration: "none" }}>
+                                        <HiExternalLink style={{ fontSize: "24px" }} />
+                                        <span>&nbsp;&nbsp;&nbsp;Live</span>
+                                    </a>
+                                </Button>
+                            </Col>
+                        </Row>
+                    </Col>
+                    <Col sm={6} style={{ padding: "0px" }}>
+                        <img src={typeTestImg} className="ProjectImage" />
+                    </Col>
+                </Row>
+            </Slide>
+            {/* <Slide right>
+                <Row className="indProj">
+                    <Col sm={6} style={{ padding: "0px" }}>
+                        <img src={expmngr} className="ProjectImage" />
+                    </Col>
+                    <Col sm={6} className="indProjDes">
+                        <h2 style={{ padding: "10px", marginBottom: "20px" }}>Expense Manager</h2>
+                        <h5>
+                            Managing everyday expenses is the first step to mastering your financial stability. This Expense manager lets you add your everyday expenses and
+                            categorize them and sort them by price every month. It also comes with an interactive dashboard of your monthly and yearly expenses.
+                        </h5>
+                        <Row style={{ padding: "10px 0px" }}>
+                            <Col xs={6}>
+                                <Button variant="primary" className="projBtn">
+                                    <a href="https://github.com/itsprathapkreddy/ExpenseManager" target="_blank" style={{ color: "inherit", textDecoration: "none" }}>
+                                        <SiGithub style={{ fontSize: "24px" }} />
+                                        <span>&nbsp;&nbsp;&nbsp;Github</span>
+                                    </a>
+                                </Button>
+                            </Col>
+                            <Col xs={6}>
+                                <Button variant="success" className="projBtn">
+                                    <a href="https://expensemanager-993c2.web.app/dashboard" target="_blank" style={{ color: "inherit", textDecoration: "none" }}>
+                                        <HiExternalLink style={{ fontSize: "24px" }} />
+                                        <span>&nbsp;&nbsp;&nbsp;Live</span>
+                                    </a>
+                                </Button>
+                            </Col>
+                        </Row>
+                    </Col>
+                </Row>
+            </Slide> */}
+        </Container>
+    );
 };
 export default Projects;
